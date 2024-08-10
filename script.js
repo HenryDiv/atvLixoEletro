a1170a28a1a617c7d6bccf1e8b7748b35a579c51
henry4i20 <henrimito1305@gmail.com>
2024-08-10 13:37:53 -0300

    receba

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
-function clickLogo(){
-    location.reload();
+function clickLogo() {
+  location.reload();
 }
+
+window.revelar = ScrollReveal({ reset: true });
+
+
+revelar.reveal(".efeito01-esq", {
+  duration: 2000,
+  distance: "100px",
+  delay: 200,
+});
+
+revelar.reveal(".efeito01-dir", {
+  duration: 2000,
+  distance: "100px",
+});
+
+revelar.reveal(".efeito03-dir", {
+  duration: 2000,
+  distance: "100px",
+  delay: 200,
+});
+
+revelar.reveal(".efeito03-esq", {
+  duration: 2000,
+  distance: "100px",
+});
+
+revelar.reveal(".efeito04-txt", {
+  duration: 2000,
+  distance: "100px",
+});
+
+revelar.reveal(".efeito04-map", {
+  duration: 2000,
+  distance: "100px",
+});
